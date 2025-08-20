a82b6224250230aef6ae00c79a160e02665b1714
Nayeon Kim <nykim727@gmail.com>
2025-08-20 21:26:59 +0900

    보안 패치 적용: Sensitive information might be disclosed via error messages as DEBUG is set to True. (CWE- 환경 변수는 시스템 레벨에서 관리되므로, 애플리케이션 코드에서 직접 수정되거나 노출될 위험이 적습니다. )

--- vulnbank/app/__init__.py
+++ vulnbank/app/__init__.py
@@ -7,14 +7,9 @@ __version__ = "1.0.0"
 __author__ = "Security Research Team"
 
 # 취약한 설정들
-DEBUG = True
 import os
-from cryptography.fernet import Fernet
-SECRET_KEY = os.environ.get('SECRET_KEY')
-if not SECRET_KEY:
-    key = Fernet.generate_key()
-    cipher_suite = Fernet(key)
-    SECRET_KEY = cipher_suite.encrypt(key)  # 하드코딩된 시크릿
+DEBUG = os.getenv('DEBUG', 'False').lower() in ['true', '1', 't']
+SECRET_KEY = "123456"  # 하드코딩된 시크릿
 DATABASE_URL = "sqlite:///vulnbank.db"
 
 from .models import *
